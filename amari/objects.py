83bcd1731eea616ce3255de5c1f75a331bb2fae6
TheF1ng3r <51931914+TheF1ng3r@users.noreply.github.com>
2021-08-31 21:35:46 -0400

    Added position slot + added users in leaderboard object

--- amari/objects.py
+++ amari/objects.py
@@ -1,15 +1,19 @@
 __all__ = ("User", "Leaderboard", "Rewards")
 
-class User:
-    __slots__ = ("guild_id", "user_id", "name", "exp", "level", "weeklyexp")
 
-    def __init__(self, guild_id: int, data: dict):
+class User:
+    __slots__ = ("guild_id", "user_id", "name", "exp", "level", "weeklyexp", "position")
+
+    def __init__(self, guild_id: int, data: dict, position: int = None):
         self.guild_id: int = guild_id
         self.user_id: int = int(data["id"])
         self.name: str = data["username"]
         self.exp: int = int(data["exp"])
-        self.level: int = int(data["level"])
-        self.weeklyexp: int = int(data["weeklyExp"])
+        self.level: int = int(data.get("level")) if data.get("level") else None
+        self.weeklyexp: int = (
+            int(data.get("weeklyExp")) if data.get("weeklyExp") else None
+        )
+        self.position: int = position
 
 
 class Leaderboard:
@@ -18,13 +22,16 @@ class Leaderboard:
     def __init__(self, guild_id: int, data: dict):
         self.guild_id: int = guild_id
         self.total_count: int = int(data["total_count"])
-        self.leaderboard = data["data"] # TODO parse into dict of partial users with cached property
+        self.leaderboard = [
+            User(guild_id, user, position) for position, user in enumerate(data["data"])
+        ]
 
     def __repr__(self) -> str:
         return f"<Leaderboard guild_id={self.guild_id} total_count={self.total_count}>"
 
     def __len__(self) -> int:
         return self.total_count
+
 
 class Rewards:
     __slots__ = ("id", "count", "roles")
