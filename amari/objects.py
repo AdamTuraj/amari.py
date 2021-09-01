4367df89e6e15c769a2b8ad18477b8c6b4f1755b
TheF1ng3r <adamturaj2@gmail.com>
2021-09-01 08:22:20 -0400

    Added session closed + updates to objects

--- amari/objects.py
+++ amari/objects.py
@@ -1,45 +1,51 @@
+__all__ = ("User", "Leaderboard", "Rewards")
+
+
 class User:
-    __slots__ = ("id", "name", "exp", "level", "weeklyexp")
+    __slots__ = ("guild_id", "user_id", "name", "exp", "level", "weeklyexp", "position")
 
-    def __init__(self, data):
-        self.set_data(data)
+    def __init__(self, guild_id: int, data: dict, position: int = None):
+        self.guild_id: int = guild_id
+        self.user_id: int = int(data["id"])
+        self.name: str = data["username"]
+        self.exp: int = int(data["exp"])
+        self.level: int = int(data.get("level")) if data.get("level") else None
+        self.weeklyexp: int = (
+            int(data.get("weeklyExp")) if data.get("weeklyExp") else None
+        )
+        self.position: int = position
 
-    def __str__(self) -> str:
-        return self.name
-
-    def set_data(self, data):
-        self.id = int(data["id"])
-        self.name = data["username"]
-        self.exp = int(data["exp"])
-        self.level = int(data["level"])
-        self.weeklyexp = int(data["weeklyExp"])
+    def __repr__(self) -> str:
+        return f"<User user_id={self.user_id} exp={self.exp}>"
 
 
 class Leaderboard:
-    __slots__ = ("id", "usercount", "leaderboard")
+    __slots__ = ("guild_id", "total_count", "leaderboard")
 
-    def __init__(self, data):
-        self.set_data(data)
+    def __init__(self, guild_id: int, data: dict):
+        self.guild_id: int = guild_id
+        self.total_count: int = int(data["total_count"])
+        self.leaderboard = [
+            User(guild_id, user, position) for position, user in enumerate(data["data"])
+        ]
 
-    def __str__(self) -> str:
-        return str(self.id)
+    def __repr__(self) -> str:
+        return f"<Leaderboard guild_id={self.guild_id} total_count={self.total_count}>"
 
-    def set_data(self, data):
-        self.id = int(data["id"])
-        self.usercount = int(data["total_count"])
-        self.leaderboard = data["data"]
+    def __len__(self) -> int:
+        return self.total_count
 
 
 class Rewards:
-    __slots__ = ("id", "count", "roles")
+    __slots__ = ("guild_id", "reward_count", "rewards")
 
-    def __init__(self, data):
-        self.set_data(data)
+    def __init__(self, guild_id: int, data: dict):
+        self.guild_id: int = guild_id
+        self.reward_count: int = int(data["count"])
+        self.rewards = [(role["level"], role["roleID"]) for role in data["data"]]
 
-    def __str__(self) -> str:
-        return str(self.id)
+    def __repr__(self) -> str:
+        return f"<Rewards guild_id={self.guild_id} reward_count = {self.reward_count}>"
 
-    def set_data(self, data):
-        self.id = int(data["id"])
-        self.count = int(data["count"])
-        self.roles = [(role["level"], role["roleID"]) for role in data["data"]]
+    def __len__(self) -> int:
+        return self.reward_count
