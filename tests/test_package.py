dac2a73bb6d25bdd88e38fa7518562fac4d67519
Adam Turaj <adamturaj2@gmail.com>
2022-03-21 18:58:49 -0400

    Accidently leaked my token

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,13 +1,15 @@
 from amari import AmariClient as Amari
 import pytest
 
-token = "a8a7a409096e7d9d9daee709.50964a.72fdcc24868e6f1c0f8ae2e2b02"
+import os
+
+TOKEN = os.getenv("AMARI_TOKEN")
 
 
 @pytest.mark.asyncio
 async def test_users():
     """Tests the fetching users function"""
-    client = Amari(token)
+    client = Amari(TOKEN)
 
     try:
         assert await client.fetch_user(346474194394939393, 374147012599218176)
@@ -22,7 +24,7 @@ async def test_users():
 async def test_leaderboards():
     """Tests the fetching leaderboards function"""
 
-    client = Amari(token)
+    client = Amari(TOKEN)
 
     try:
         assert await client.fetch_leaderboard(346474194394939393, page=3, limit=100)
@@ -35,7 +37,7 @@ async def test_leaderboards():
 async def test_rewards():
     """Tests the fetching rewards function"""
 
-    client = Amari(token)
+    client = Amari(TOKEN)
 
     try:
         assert await client.fetch_rewards(346474194394939393, page=1, limit=20)
